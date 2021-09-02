27994bb1592f182dc3df48001dbed462d37fa671
parsgit <nvioir@gmail.com>
2021-09-03 00:07:38 +0430

    add notFound method

--- src/Route.php
+++ src/Route.php
@@ -6,8 +6,11 @@ use webrium\core\Route as IRoute;
 
 class Route {
 
+  public static $find = false;
+
   public static function text($path, $controllerString){
     if (!bot::getCallback() && $path == bot::text()) {
+      self::$find = true;
       IRoute::call($controllerString);
     }
   }
@@ -17,6 +20,7 @@ class Route {
     $data = self::findVar(bot::data(),$path);
 
     if (bot::getCallback() && $data['is']) {
+      self::$find = true;
       IRoute::call($controllerString);
     }
   }
@@ -30,24 +34,37 @@ class Route {
 
       if (strpos($str,'{')===0) {
         $newStr = str_replace('{','',$str);
-        $newStr = str_replace('}','',$newStr);
+          $newStr = str_replace('}','',$newStr);
 
-        if (isset($arr[$key])) {
-          $params[$newStr] = $arr[$key];
+          if (isset($arr[$key])) {
+            $params[$newStr] = $arr[$key];
 
-          $path = str_replace('{'.$newStr.'}',$arr[$key],$path);
+            $path = str_replace('{'.$newStr.'}',$arr[$key],$path);
+          }
+
+        }
+      }
+      $is = false;
+      if ($string===$path) {
+        $is = true;
+      }
+
+      bot::$params = $params;
+
+      return ['params'=>$params,'is'=>$is];
+    }
+
+
+    public static function notFound($file=false)
+    {
+      if (!self::$find) {
+        if ( $file) {
+          IRoute::call($file);
         }
 
+        die;
       }
-    }
-    $is = false;
-    if ($string===$path) {
-      $is = true;
+
     }
 
-    bot::$params = $params;
-
-    return ['params'=>$params,'is'=>$is];
   }
-
-}
