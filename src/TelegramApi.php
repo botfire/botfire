21028fdd52c133576dbf6c6f30d17ed540239fe1
parsgit <nvioir@gmail.com>
2025-05-19 14:33:58 +0000

    fix: update TelegramApi to handle params directly and disable SSL verification

--- src/TelegramApi.php
+++ src/TelegramApi.php
@@ -7,8 +7,8 @@ class TelegramApi {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         curl_close($ch);
         return json_decode($response, true);
