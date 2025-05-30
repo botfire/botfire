3d1d23b9fc51ad85d5103634e92c31a264c7119d
parsgit <nvioir@gmail.com>
2025-05-30 16:45:08 +0000

    feat: refactor Voice class to extend Option and integrate OptionCaption traits; add VoiceResult class for handling voice file data

--- src/Models/Voice.php
+++ src/Models/Voice.php
@@ -1,23 +1,35 @@
 <?php
 namespace Botfire\Models;
-use Botfire\Models\File;
+use Botfire\Models\OptionCaption;
 
-class Voice extends File{
-    protected $data;
 
-    public function __construct($data) {
-        $this->data = $data;
+
+class Voice extends Option{
+
+    use OptionCaption;
+
+    protected $data = [];
+
+
+    /**
+     * Audio file to send. Pass a file_id as String to send a file that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data
+     * @param mixed $voice
+     */
+    public function __construct($voice){
+        $this->data['voice'] = $voice;
     }
 
-    public function getDuration() {
-        return $this->data['duration'] ?? null;
+
+
+    /**
+     * Duration of the audio in seconds
+     * @param int $duration
+     * @return static
+     */
+    public function duration(int $duration) {
+        $this->data['duration'] = $duration;
+        return $this;
     }
 
-    public function getMimeType() {
-        return $this->data['mime_type'] ?? null;
-    }
 
-    public function getFileSize() {
-        return $this->data['file_size'] ?? null;
-    }
 }
