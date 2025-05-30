fc8c0cf21f7b80428a528639a32596adc9d89788
parsgit <nvioir@gmail.com>
2025-05-30 16:46:14 +0000

    feat: add methods for sending various media types including video notes, videos, photos, voices, audios, documents, and messages

--- src/Bot.php
+++ src/Bot.php
@@ -1,6 +1,15 @@
 <?php
 namespace Botfire;
 
+use Botfire\Models\Audio;
+use Botfire\Models\Document;
+use Botfire\Models\Message;
+use Botfire\Models\Photo;
+use Botfire\Models\Video;
+use Botfire\Models\VideoNote;
+use Botfire\Models\Voice;
+use Botfire\Message as GetMessage;
+
 class Bot
 {
     private static $instance = null;
@@ -61,15 +70,15 @@ class Bot
 
             $input = self::getInput();
 
-            if (isset($input[Message::TYPE_CALLBACK_QUERY])) {
-                $message = ['message' => $input[Message::TYPE_CALLBACK_QUERY]['message']];
-                unset($input[Message::TYPE_CALLBACK_QUERY]['message']);
-                $callback = $input[Message::TYPE_CALLBACK_QUERY];
+            if (isset($input[GetMessage::TYPE_CALLBACK_QUERY])) {
+                $message = ['message' => $input[GetMessage::TYPE_CALLBACK_QUERY]['message']];
+                unset($input[GetMessage::TYPE_CALLBACK_QUERY]['message']);
+                $callback = $input[GetMessage::TYPE_CALLBACK_QUERY];
 
-                $bot->message = new Message($message);
+                $bot->message = new GetMessage($message);
                 $bot->callback = new Callback($callback);
             } else {
-                $bot->message = new Message($input);
+                $bot->message = new GetMessage($input);
                 $bot->callback = null;
             }
 
@@ -89,8 +98,52 @@ class Bot
         $bot = new self();
         $bot->message = new Message(self::getInput());
         $chat = $bot->message()->chat()->asArray();
-        return new Message(['message' => ['chat' => $chat]]);
+        return new NewMessage(['message' => ['chat' => $chat]]);
     }
+
+    
+
+
+    public static function sendVideoNote(VideoNote|string $videoNote){
+        $bot = self::new();
+        return $bot->videoNote($videoNote)->send();
+    }
+
+    public static function sendVideo(Video|string $video){
+        $bot = self::new();
+        return $bot->video($video)->send();
+    }
+
+    public static function sendPhoto(Photo|string $photo){
+        $bot = self::new();
+        return $bot->photo($photo)->send();
+    }
+
+    public static function sendVoice(Voice|string $voice){
+        $bot = self::new();
+        return $bot->voice($voice)->send();
+    }
+
+    public static function sendAudio(Audio|string $audio){
+        $bot = self::new();
+        return $bot->audio($audio)->send();
+    }
+
+
+    public static function sendDocument(Document|string $document){
+        $bot = self::new();
+        return $bot->document($document)->send();
+    }
+
+
+    public static function sendMessage(Message|string $text){
+        $bot = self::new();
+        return $bot->text($text)->send();
+    }
+
+
+    
+
 
 
     public static function callback()
