90dbe22d6d0dfaa75b9ecc5c4cfdab9410a4cefb
parsgit <nvioir@gmail.com>
2025-06-01 14:03:25 +0000

    Add WebAppInfo class to represent Web App information with URL validation

---
+++ src/Models/WebAppInfo.php
@@ -0,0 +1,55 @@
+<?php
+namespace Botfire\Models;
+
+/**
+ * Represents information about a Web App.
+ */
+class WebAppInfo {
+    /**
+     * @var string $url An HTTPS URL of a Web App to be opened with additional data as specified in Telegram's documentation.
+     */
+    private string $url;
+
+    /**
+     * Constructor to initialize the WebAppInfo object.
+     *
+     * @param string $url An HTTPS URL of a Web App.
+     */
+    public function __construct(string $url) {
+        $this->setUrl($url);
+    }
+
+    /**
+     * Get the Web App URL.
+     *
+     * @return string
+     */
+    public function getUrl(): string {
+        return $this->url;
+    }
+
+    /**
+     * Set the Web App URL.
+     *
+     * @param string $url An HTTPS URL of a Web App.
+     * @return void
+     */
+    public function setUrl(string $url): void {
+        if (!filter_var($url, FILTER_VALIDATE_URL) || stripos($url, 'https://') !== 0) {
+            throw new \InvalidArgumentException("The URL must be a valid HTTPS URL.");
+        }
+        $this->url = $url;
+    }
+
+
+    /**
+     * Convert the WebAppInfo object to an array for API requests.
+     *
+     * @return array
+     */
+    public function toArray(): array {
+        return [
+            'url' => $this->url,
+        ];
+    }
+}
