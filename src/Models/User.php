2bf6f4a928be8d63326123090ce9d27cf888a186
parsgit <nvioir@gmail.com>
2025-06-01 22:14:39 +0000

    Rename getID method to getId for consistency with naming conventions

--- src/Models/User.php
+++ src/Models/User.php
@@ -21,7 +21,7 @@ class User
      * This number may have more than 32 significant bits and some programming languages may have difficulty/silent defects in interpreting it.
      * But it has at most 52 significant bits, so a 64-bit integer or double-precision float type are safe for storing this identifier.
      */
-    public function getID(): ?int
+    public function getId(): ?int
     {
         return $this->data['id'] ?? null;
     }
