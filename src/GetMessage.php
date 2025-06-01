1907e6eae65830deeed00f02bb8da592c0cf6dda
parsgit <nvioir@gmail.com>
2025-06-01 22:23:56 +0000

    Add date method to GetMessage class and remove unused methods for cleaner code

--- src/GetMessage.php
+++ src/GetMessage.php
@@ -69,6 +69,11 @@ class GetMessage
         return $this->data['message']['message_id'] ?? null;
     }
 
+    public function date()
+    {
+        return $this->data['message']['date'] ?? null;
+    }
+
     public function from()
     {
         return new User($this->data['message']['from'] ?? []);
@@ -84,137 +89,13 @@ class GetMessage
 
         return $this->data['message']['text'] ?? null;
     }
-    
 
-    public function photo($photo = null)
+
+    public function caption()
     {
-        if ($photo !== null) {
-            $this->sendParams['photo'] = $photo;
-            $this->sendMethod = 'photo';
-            return $this;
-        }
-
-        $photoArray = new PhotoArray($this->data['message']['photo'] ?? []);
-        return $photoArray;
-    }
-
-
-
-
-
-    /**
-     * Use this method to send audio files,
-     * if you want Telegram clients to display them in the music player.
-     * Your audio must be in the .MP3 or .M4A format. On success, the sent Message is returned.
-     * Bots can currently send audio files of up to 50 MB in size, this limit may be changed in the future.
-     * 
-     * For sending voice messages, use the voice method instead.
-     * 
-     * @param \Botfire\Models\Audio|string $audio
-     * @return static
-     */
-    public function audio(Audio|string $audio)
-    {
-
-        if ($audio instanceof Audio) {
-            $audio->appendToSendParams($this->sendParams);
-        } else {
-            $this->sendParams['audio'] = $audio;
-        }
-
-        $this->sendMethod = 'audio';
-
-        return $this;
-    }
-
-
-    /**
-     * Use this method to send general files.
-     * On success, the sent Message is returned.
-     * Bots can currently send files of any type of up to 50 MB in size, this limit may be changed in the future.
-     * @param \Botfire\Models\Document|string $document
-     * @return static
-     */
-    public function document(Document|string $document)
-    {
-
-        if ($document instanceof Document) {
-            $document->appendToSendParams($this->sendParams);
-        } else {
-            $this->sendParams['document'] = $document;
-        }
-
-        $this->sendMethod = 'document';
-
-        return $this;
-    }
-
-
-    public function video(Video|string $video = null)
-    {
-        if ($video instanceof Video) {
-            $video->appendToSendParams($this->sendParams);
-        } else {
-            $this->sendParams['document'] = $video;
-        }
-
-        $this->sendMethod = 'document';
-
-        return $this;
-    }
-
-
-    public function caption($caption = null)
-    {
-        if ($caption !== null) {
-            $this->sendParams['caption'] = $caption;
-            return $this;
-        }
         return $this->data['message']['caption'] ?? null;
     }
 
 
-    public function thumbnail($thumbnail)
-    {
-        $this->sendParams['thumbnail'] = $thumbnail;
-        return $this;
-    }
-
-
-
-    /**
-     * Send Message Or Action
-     * @param string|int $chat_id Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-     */
-    public function send(string|int $chat_id = null)
-    {
-
-        $this->sendParams['chat_id'] = $chat_id ?? $this->chat()->id();
-        file_put_contents(__DIR__ . '/send.log', "sendMethod : " . $this->sendMethod . "  make:" . $this->makeMethodName($this->sendMethod));
-        return Bot::getParser()->request($this->makeMethodName($this->sendMethod), $this->sendParams);
-    }
-
-    public function edit($message_id = null)
-    {
-        $params = $this->sendParams;
-        $params['chat_id'] = $this->chat()->id();
-        $params['message_id'] = $message_id ?? $this->messageId();
-        return Bot::getParser()->request('editMessageText', $params);
-    }
-
-    public function delete($message_id = null)
-    {
-        $params = [
-            'chat_id' => $this->chat()->id(),
-            'message_id' => $message_id ?? $this->messageId()
-        ];
-        return Bot::getParser()->request('deleteMessage', $params);
-    }
-
-    public function replyMarkup($keyboard)
-    {
-        $this->sendParams['reply_markup'] = $keyboard->toArray();
-        return $this;
-    }
 }
 
