1fd28e97535c6f4a5a6a177108f459b297a9c9fa
parsgit <nvioir@gmail.com>
2025-06-01 08:10:56 +0000

    add ReplyKeyboardRemove Class

---
+++ src/ReplyKeyboardRemove.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Botfire;
+
+class ReplyKeyboardRemove{
+
+    private bool $selective = false;
+
+
+    public function setSelective(bool $selective): static {
+        $this->selective = $selective;
+        return $this;
+    }
+
+    public function toArray() {
+        return [
+            'remove_keyboard' => true,
+            'selective' => $this->selective,
+        ];
+    }
+}
