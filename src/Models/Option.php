4c8cedece7f5cc74f31b3b30ef426d5068e07244
parsgit <nvioir@gmail.com>
2025-06-01 09:43:07 +0000

    update

--- src/Models/Option.php
+++ src/Models/Option.php
@@ -47,31 +47,6 @@ class Option
 
 
 
-    // /**
-    //  * New caption for media, 0-1024 characters after entities parsing. If not specified, the original caption is kept
-    //  * @param string $caption
-    //  * @return static
-    //  */
-    // public function caption(string $caption)
-    // {
-    //     $this->data['caption'] = $caption;
-    //     return $this;
-    // }
-
-
-
-    // /**
-    //  * 	A JSON-serialized list of special entities that appear in the caption, which can be specified instead of parse_mode
-    //  * @param string $caption_entities
-    //  * @return static
-    //  */
-    // public function captionEntities($caption_entities)
-    // {
-    //     $this->data['caption_entities'] = $caption_entities;
-    //     return $this;
-    // }
-
-
 
     /**
      * Unique identifier of the business connection on behalf of which the message will be sent
