513adfe9d783c7d7d443aa5339643d64fe51cda7
parsgit <nvioir@gmail.com>
2025-06-01 07:51:40 +0000

    feat: enhance MarkupKeyboard class with additional configuration options and methods for better customization

--- src/MarkupKeyboard.php
+++ src/MarkupKeyboard.php
@@ -1,59 +1,136 @@
 <?php
 namespace Botfire;
 
-class MarkupKeyboard {
-    private $buttons = [];
-    private $currentRow = -1;
+use Botfire\Models\KeybardOption;
 
-    public function __construct() {
-        $this->row();
-    }
+class MarkupKeyboard
+{
 
-    public function btn($text) {
-        $this->buttons[$this->currentRow][] = [
-            'text' => $text
-        ];
+
+
+    protected $buttons = [];
+    private int $currentRow = 0;
+
+    private bool $selective = false;
+
+    private string $input_field_placeholder = '';
+
+    private bool $one_time_keyboard = false;
+
+    private bool $resize_keyboard = false;
+
+
+
+
+
+
+    /**
+     * Optional
+     * Use this parameter if you want to show the keyboard to specific users only.
+     * Targets: 
+     *   1) users that are @mentioned in the text of the Message object;
+     *   2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
+     * @param bool $selective
+     * @return static
+     */
+    public function selective(bool $selective)
+    {
+        $this->selective = $selective;
         return $this;
     }
 
-    public function btnUrl($text, $url) {
-        $this->buttons[$this->currentRow][] = [
-            'text' => $text,
-            'url' => $url
-        ];
+
+    /**
+     * Optional
+     * The placeholder to be shown in the input field when the keyboard is active; 1-64 characters
+     * @param string $placeholder
+     * @return static
+     */
+    public function inputFieldPlaceholder(string $placeholder)
+    {
+        $this->input_field_placeholder = $placeholder;
         return $this;
     }
 
-    public function row() {
-        $this->currentRow++;
-        $this->buttons[$this->currentRow] = [];
+
+    /**
+     * Optional
+     * Requests clients to hide the keyboard as soon as it's been used.
+     * The keyboard will still be available, but clients will automatically display the usual letter-keyboard in the chat - the user can press a special button in the input field to see the custom keyboard again.
+     * Defaults to false.
+     * @param bool $one_time_keyboard
+     * @return static
+     */
+    public function oneTimeKeyboard(bool $one_time_keyboard)
+    {
+        $this->one_time_keyboard = $one_time_keyboard;
         return $this;
     }
 
-    public function autoRow($columns) {
-        $newButtons = [];
-        $row = [];
-        foreach ($this->buttons as $buttonRow) {
-            foreach ($buttonRow as $button) {
-                $row[] = $button;
-                if (count($row) >= $columns) {
-                    $newButtons[] = $row;
-                    $row = [];
+    /**
+     * Optional
+     * Requests clients to resize the keyboard vertically for optimal fit
+     * (e.g., make the keyboard smaller if there are just two rows of buttons)
+     * Defaults to false, in which case the custom keyboard is always of the same height as the app's standard keyboard.
+     * @param bool $resize_keyboard
+     * @return static
+     */
+    public function resizeKeyboard(bool $resize_keyboard)
+    {
+        $this->resize_keyboard = $resize_keyboard;
+        return $this;
+    }
+
+
+
+    public function row(array $buttons = [])
+    {
+        if (!empty($buttons)) {
+            $this->buttons[] = array_map(function ($button) {
+                if ($button instanceof KeyboardButton) {
+                    return $button->toArray();
                 }
-            }
+                return ['text' => $button];
+            }, $buttons);
+            $this->currentRow++;
+            return $this;
         }
-        if ($row) {
-            $newButtons[] = $row;
+
+        // Start a new row
+        if ($this->currentRow >= 0 && !empty($this->buttons[$this->currentRow])) {
+            $this->currentRow++;
         }
-        $this->buttons = $newButtons;
+
+        // $this->buttons[] = [];
         return $this;
     }
 
-    public function toArray() {
-        return [
+
+    /**
+     * Convert the keyboard to an array format suitable for Telegram API.
+     * @return array{keyboard: array, one_time_keyboard: bool, resize_keyboard: bool}
+     */
+    public function toArray()
+    {
+        $result = [
             'keyboard' => array_filter($this->buttons),
-            'resize_keyboard' => true,
-            'one_time_keyboard' => true
+            'resize_keyboard' => $this->resize_keyboard,
+            'one_time_keyboard' => $this->one_time_keyboard,
         ];
+
+        if ($this->selective) {
+            $result['selective'] = $this->selective;
+        }
+
+        if (!empty($this->input_field_placeholder)) {
+            $result['input_field_placeholder'] = $this->input_field_placeholder;
+        }
+
+        return $result;
+    }
+
+
+    public function toJson(){
+        return json_encode($this->toArray(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 }
