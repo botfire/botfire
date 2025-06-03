4d0c8d15cbd9680f05330048e5d1ff35b4d0cd3f
parsgit <nvioir@gmail.com>
2025-06-03 18:02:11 +0000

    Refactor Animation class by removing unused methods and adding relevant traits for enhanced functionality

--- src/Models/Animation.php
+++ src/Models/Animation.php
@@ -1,8 +1,35 @@
 <?php
 namespace Botfire\Models;
-use Botfire\Models\File;
 
-class Animation extends Option{
+use Botfire\TraitMethods\AllowPaidBroadcastTrait;
+use Botfire\TraitMethods\BusinessConnectionIdTrait;
+use Botfire\TraitMethods\CaptionEntitiesTrait;
+use Botfire\TraitMethods\CaptionTrait;
+use Botfire\TraitMethods\DisableNotificationTrait;
+use Botfire\TraitMethods\DurationTrait;
+use Botfire\TraitMethods\HasSpoilerTrait;
+use Botfire\TraitMethods\HeightTrait;
+use Botfire\TraitMethods\MessageEffectIdTrait;
+use Botfire\TraitMethods\MessageThreadIdTrait;
+use Botfire\TraitMethods\ParseModeTrait;
+use Botfire\TraitMethods\ProtectContentTrait;
+use Botfire\TraitMethods\ReplyMarkupTrait;
+use Botfire\TraitMethods\ReplyParametersTrait;
+use Botfire\TraitMethods\ShowCaptionAboveMediaTrait;
+use Botfire\TraitMethods\ThumbnailTrait;
+use Botfire\TraitMethods\WidthTrait;
+
+class Animation extends Option
+{
+
+
+    use BusinessConnectionIdTrait, MessageThreadIdTrait, ThumbnailTrait;
+    use CaptionTrait, ParseModeTrait, CaptionEntitiesTrait;
+    use DisableNotificationTrait, ProtectContentTrait, AllowPaidBroadcastTrait;
+    use MessageEffectIdTrait, ReplyParametersTrait, ReplyMarkupTrait;
+    use DurationTrait, WidthTrait, HeightTrait, ShowCaptionAboveMediaTrait, HasSpoilerTrait;
+
+
 
     protected $data = [];
 
@@ -11,65 +38,9 @@ class Animation extends Option{
      * Use this method to send animation files (GIF or H.264/MPEG-4 AVC video without sound). On success, the sent Message is returned. Bots can currently send animation files of up to 50 MB in size, this limit may be changed in the future
      * @param mixed $audio
      */
-    public function __construct($audio){
+    public function __construct($audio)
+    {
         $this->data['animation'] = $audio;
     }
-
-
-
-    /**
-     * Animation width
-     * @param int $width
-     * @return static
-     */
-    public function width(int $width){
-        $this->data['width'] = $width;
-        return $this;
-    }
-
-
-    /**
-     * Animation height
-     * @param int $height
-     * @return static
-     */
-    public function height(int $height){
-        $this->data['height'] = $height;
-        return $this;
-    }
-
-
-    /**
-     * Duration of the audio in seconds
-     * @param int $duration
-     * @return static
-     */
-    public function duration(int $duration) {
-        $this->data['duration'] = $duration;
-        return $this;
-    }
-
-    
-
-    /**
-     * Pass True, if the caption must be shown above the message media
-     * @param bool $show_caption_above_media
-     * @return static
-     */
-    public function showCaptionAboveAedia(bool $show_caption_above_media){
-        $this->data['show_caption_above_media'] = $show_caption_above_media;
-        return $this;
-    }    
-
-    /**
-     * Pass True if the animation needs to be covered with a spoiler animation
-     * @param bool $has_spoiler
-     * @return static
-     */
-    public function hasSpoiler(bool $has_spoiler){
-        $this->data['has_spoiler'] = $has_spoiler;
-        return $this;
-    }
-
 
 }
