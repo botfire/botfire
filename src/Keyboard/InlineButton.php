15e3bfa378e4ce74d079285141742c8ab68a0cc9
parsgit <nvioir@gmail.com>
2025-06-03 19:50:06 +0000

    Refactor InlineButton class to use a data array for properties and simplify button creation methods

--- src/Keyboard/InlineButton.php
+++ src/Keyboard/InlineButton.php
@@ -8,126 +8,105 @@ use Botfire\Models\WebAppInfo;
 
 class InlineButton
 {
-    protected string $text;
-    protected ?string $url = null;
-    protected ?string $callback_data = null;
-    protected ?WebAppInfo $web_app = null;
-    protected ?LoginUrl $login_url = null;
-    protected ?string $switch_inline_query = null;
-    protected ?string $switch_inline_query_current_chat = null;
-    protected ?SwitchInlineQueryChosenChat $switch_inline_query_chosen_chat = null;
-    protected ?CopyTextButton $copy_text = null;
-    protected $callback_game = null;
-    protected ?bool $pay = null;
+    protected array $data = [];
 
-
-
-    public function url(string $text, string $url): static
+    public static function btnUrl(string $text, string $url): static
     {
-        $this->text = $text;
-        $this->url = $url;
+        $btn = new static();
+        $btn->text($text);
+        $btn->url($url);
+        return $btn;
+    }
+
+    public static function btn(string $text, string $callback_data): static
+    {
+        $btn = new static();
+        $btn->text($text);
+        $btn->callbackData($callback_data);
+        return $btn;
+    }
+
+    public function text(string $text): static
+    {
+        $this->data['text'] = $text;
         return $this;
     }
 
-    public function btn(string $text, string $callback_data): static
+    public function url(?string $url): static
     {
-        $this->text = $text;
-        $this->callback_data = $callback_data;
+        $this->data['url'] = $url;
+        return $this;
+    }
+
+    public function callbackData(?string $callback_data): static
+    {
+        $this->data['callback_data'] = $callback_data;
         return $this;
     }
 
     public function webApp(?WebAppInfo $web_app): static
     {
-        $this->web_app = $web_app;
+        $this->data['web_app'] = $web_app;
         return $this;
     }
 
     public function loginUrl(?LoginUrl $login_url): static
     {
-        $this->login_url = $login_url;
+        $this->data['login_url'] = $login_url;
         return $this;
     }
 
     public function switchInlineQuery(?string $switch_inline_query): static
     {
-        $this->switch_inline_query = $switch_inline_query;
+        $this->data['switch_inline_query'] = $switch_inline_query;
         return $this;
     }
 
     public function switchInlineQueryCurrentChat(?string $switch_inline_query_current_chat): static
     {
-        $this->switch_inline_query_current_chat = $switch_inline_query_current_chat;
+        $this->data['switch_inline_query_current_chat'] = $switch_inline_query_current_chat;
         return $this;
     }
 
     public function switchInlineQueryChosenChat(?SwitchInlineQueryChosenChat $switch_inline_query_chosen_chat): static
     {
-        $this->switch_inline_query_chosen_chat = $switch_inline_query_chosen_chat;
+        $this->data['switch_inline_query_chosen_chat'] = $switch_inline_query_chosen_chat;
         return $this;
     }
 
     public function copyText(?CopyTextButton $copy_text): static
     {
-        $this->copy_text = $copy_text;
+        $this->data['copy_text'] = $copy_text;
         return $this;
     }
 
     public function callbackGame($callback_game): static
     {
-        $this->callback_game = $callback_game;
+        $this->data['callback_game'] = $callback_game;
         return $this;
     }
 
     public function pay(?bool $pay): static
     {
-        $this->pay = $pay;
+        $this->data['pay'] = $pay;
         return $this;
     }
 
     public function toArray(): array
     {
-        $button = [
-            'text' => $this->text,
-        ];
+        $button = [];
 
-        if ($this->url) {
-            $button['url'] = $this->url;
-        }
+        foreach ($this->data as $key => $value) {
+            if ($value === null) {
+                continue;
+            }
 
-        if ($this->callback_data) {
-            $button['callback_data'] = $this->callback_data;
-        }
-
-        if ($this->web_app) {
-            $button['web_app'] = $this->web_app->toArray();
-        }
-
-        if ($this->login_url) {
-            $button['login_url'] = $this->login_url->toArray();
-        }
-
-        if ($this->switch_inline_query) {
-            $button['switch_inline_query'] = $this->switch_inline_query;
-        }
-
-        if ($this->switch_inline_query_current_chat) {
-            $button['switch_inline_query_current_chat'] = $this->switch_inline_query_current_chat;
-        }
-
-        if ($this->switch_inline_query_chosen_chat) {
-            $button['switch_inline_query_chosen_chat'] = $this->switch_inline_query_chosen_chat->toArray();
-        }
-
-        if ($this->copy_text) {
-            $button['copy_text'] = $this->copy_text->toArray();
-        }
-
-        if ($this->callback_game) {
-            $button['callback_game'] = $this->callback_game->toArray();
-        }
-
-        if ($this->pay) {
-            $button['pay'] = $this->pay;
+            // Handle objects with toArray method
+            if (is_object($value) && method_exists($value, 'toArray')) {
+                $button[$key] = $value->toArray();
+            } else {
+                $button[$key] = $value;
+            }
         }
 
         return $button;
