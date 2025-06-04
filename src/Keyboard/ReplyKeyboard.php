ce44919aaf03cb3a2c6ca5adc6e416841a6073de
parsgit <nvioir@gmail.com>
2025-06-04 11:30:23 +0000

    Refactor ReplyKeyboard class to use ReplyButton and enhance documentation

--- src/Keyboard/ReplyKeyboard.php
+++ src/Keyboard/ReplyKeyboard.php
@@ -1,22 +1,55 @@
 <?php
 namespace Botfire\Keyboard;
+use Botfire\Keyboard\ReplyButton;
 
-use Botfire\Keyboard\Button;
+/**
+ * Represents a custom keyboard with reply buttons.
+ * This class allows you to create a keyboard with multiple rows of buttons,
+ * each button can be a text button or an instance of ReplyButton.
+ *
+ * @see https://core.telegram.org/bots/api#replykeyboardmarkup
+ */
 
 class ReplyKeyboard
 {
 
 
 
-    protected $buttons = [];
+    private $buttons = [];
     private int $currentRow = 0;
 
+    /**
+     * Optional
+     * Requests clients to display the keyboard as a reply interface instead of a custom keyboard.
+     * Defaults to false, in which case the custom keyboard is always displayed.
+     * @var bool
+     */
     private bool $selective = false;
 
+
+    /**
+     * Optional
+     * The placeholder to be shown in the input field when the keyboard is active; 1-64 characters
+     * @var string
+     */
     private string $input_field_placeholder = '';
 
+    /**
+     * Optional
+     * Requests clients to hide the keyboard as soon as it's been used.
+     * The keyboard will still be available, but clients will automatically display the usual letter-keyboard in the chat - the user can press a special button in the input field to see the custom keyboard again.
+     * Defaults to false.
+     * @var bool
+     */
     private bool $one_time_keyboard = false;
 
+    /**
+     * Optional
+     * Requests clients to resize the keyboard vertically for optimal fit
+     * (e.g., make the keyboard smaller if there are just two rows of buttons)
+     * Defaults to false, in which case the custom keyboard is always of the same height as the app's standard keyboard.
+     * @var bool
+     */
     private bool $resize_keyboard = false;
 
 
@@ -87,7 +120,7 @@ class ReplyKeyboard
     {
         if (!empty($buttons)) {
             $this->buttons[] = array_map(function ($button) {
-                if ($button instanceof Button) {
+                if ($button instanceof ReplyButton) {
                     return $button->toArray();
                 }
                 return ['text' => $button];
@@ -101,7 +134,6 @@ class ReplyKeyboard
             $this->currentRow++;
         }
 
-        // $this->buttons[] = [];
         return $this;
     }
 
@@ -110,7 +142,7 @@ class ReplyKeyboard
      * Convert the keyboard to an array format suitable for Telegram API.
      * @return array{keyboard: array, one_time_keyboard: bool, resize_keyboard: bool}
      */
-    public function toArray()
+    public function toArray(): array
     {
         $result = [
             'keyboard' => array_filter($this->buttons),
@@ -130,7 +162,14 @@ class ReplyKeyboard
     }
 
 
-    public function toJson(){
+    /**
+     * Convert the keyboard to JSON format.
+     * This method encodes the keyboard to JSON format, which is suitable for sending to the Telegram API.
+     * It uses the `toArray` method to get the array representation of the keyboard and then encodes it.
+     * @return bool|string
+     */
+    public function toJson(): bool|string
+    {
         return json_encode($this->toArray(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     }
 }
