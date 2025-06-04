d1e891f23164dd95a0e1e67c966dc80d274d3539
parsgit <nvioir@gmail.com>
2025-06-04 11:04:47 +0000

    Add method to edit message reply markup

--- src/Bot.php
+++ src/Bot.php
@@ -7,6 +7,7 @@ use Botfire\Models\CopyMessages;
 use Botfire\Models\Document;
 use Botfire\Models\EditCaption;
 use Botfire\Models\EditMessage;
+use Botfire\Models\EditReplyMarkup;
 use Botfire\Models\EditText;
 use Botfire\Models\Message;
 use Botfire\Models\Photo;
@@ -277,6 +278,19 @@ class Bot
     {
         $bot = self::new();
         return $bot->editMessageCaption($caption)->send();
+    }
+
+
+    /**
+     * Use this method to edit only the reply markup of messages.
+     * On success, if the edited message is not an inline message, the edited Message is returned, otherwise True is returned.
+     * Note that business messages that were not sent by the bot and do not contain an inline keyboard can only be edited within 48 hours from the time they were sent.
+     * @param EditReplyMarkup $reply_markup
+     */
+    public static function editMessageReplyMarkup(EditReplyMarkup $reply_markup)
+    {
+        $bot = self::new();
+        return $bot->editMessageReplyMarkup($reply_markup)->send();
     }
 
 
