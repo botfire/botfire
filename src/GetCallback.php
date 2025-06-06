44fdff49d26e59bbc717550af87641dcee4212d7
parsgit <nvioir@gmail.com>
2025-06-06 13:12:14 +0000

    Enhance GetCallback class with improved documentation and type hints for methods

--- src/GetCallback.php
+++ src/GetCallback.php
@@ -4,26 +4,64 @@ namespace Botfire;
 use Botfire\Models\From;
 use Botfire\Models\User;
 
-class GetCallback {
+class GetCallback
+{
     private $data;
 
-    public function __construct($data) {
+    public function __construct($data)
+    {
         $this->data = $data;
     }
-    
-    public function id() {
+
+    /**
+     * Unique identifier for this query
+     * @return string
+     */
+    public function id(): string
+    {
         return $this->data['id'] ?? null;
     }
 
-    public function from(){
+
+    /**
+     * Sender of the message with the callback button.
+     * @return User
+     */
+    public function from()
+    {
         return new User($this->data['from'] ?? []);
     }
 
-    public function chatInstance(){
+
+    /**
+     * Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent.
+     * Useful for high scores in games.
+     * @return string
+     */
+    public function chatInstance(): ?string
+    {
         return $this->data['chat_instance'] ?? '';
     }
 
-    public function data(){
+
+    /**
+     * Optional.
+     * Identifier of the message sent via the bot in inline mode, that originated the query.
+     */
+    public function inlineMessageId(): ?string
+    {
+        return $this->data['inline_message_id'] ?? null;
+    }
+
+
+    /**
+     * Optional.
+     * Data associated with the callback button.
+     * Be aware that the message originated the query can contain no callback buttons with this data.
+     * @return string
+     */
+    public function data(): string
+    {
         return $this->data['data'] ?? '';
     }
 
