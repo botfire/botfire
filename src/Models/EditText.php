56b35d2214b7c3a67f54138b99c69bbdeccc12ff
benkhalife <nvioir@gmail.com>
2025-08-13 07:24:24 +0000

    feat: add create and send method to EditText class

--- src/Models/EditText.php
+++ src/Models/EditText.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Botfire\Models;
 
+use Botfire\Bot;
+use Botfire\Helper\MarkdownBuilder;
 use Botfire\TraitMethods\BusinessConnectionIdTrait;
 use Botfire\TraitMethods\EntitiesTrait;
 use Botfire\TraitMethods\InlineMessageIdTrait;
@@ -27,11 +30,22 @@ class EditText extends Option
      * New text of the message, 1-4096 characters after entities parsing
      * @param mixed $text
      */
-    public function __construct($text)
+    public function __construct(string|MarkdownBuilder $text)
     {
         $this->data['text'] = $text;
     }
 
 
 
+    public static function create(string|MarkdownBuilder $text): static
+    {
+        return new static($text);
+    }
+
+
+
+    public function send()
+    {
+        return Bot::editMessageText($this);
+    }
 }
