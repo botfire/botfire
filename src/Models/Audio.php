226abbea696dfc815bfe15ea5098f14f7c735636
benkhalife <nvioir@gmail.com>
2025-08-16 12:05:47 +0000

    feat: implement create and send methods in Audio class

--- src/Models/Audio.php
+++ src/Models/Audio.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Botfire\Models;
 
+use Botfire\Bot;
+use Botfire\Helper\MarkdownBuilder;
 use Botfire\TraitMethods\AllowPaidBroadcastTrait;
 use Botfire\TraitMethods\BusinessConnectionIdTrait;
 use Botfire\TraitMethods\CaptionEntitiesTrait;
@@ -12,16 +15,17 @@ use Botfire\TraitMethods\PerformerTrait;
 use Botfire\TraitMethods\ProtectContentTrait;
 use Botfire\TraitMethods\ThumbnailTrait;
 use Botfire\TraitMethods\TitleTrait;
+use CURLFile;
 
 class Audio extends Option
 {
 
-    use BusinessConnectionIdTrait,MessageThreadIdTrait;
+    use BusinessConnectionIdTrait, MessageThreadIdTrait;
     use DurationTrait, PerformerTrait;
     use CaptionAndParseModeTrait, CaptionEntitiesTrait, TitleTrait, DurationTrait, PerformerTrait;
-    use DisableNotificationTrait, ProtectContentTrait,AllowPaidBroadcastTrait;
+    use DisableNotificationTrait, ProtectContentTrait, AllowPaidBroadcastTrait;
     use ThumbnailTrait;
-    
+
 
     protected $data = [];
 
@@ -32,5 +36,27 @@ class Audio extends Option
     public function __construct($audio)
     {
         $this->data['audio'] = $audio;
+    }
+
+
+    /**
+     * Audio file to send. Pass a file_id as String to send an audio file that exists on the Telegram servers (recommended), pass an HTTP URL as a String for Telegram to get an audio file from the Internet, or upload a new one using multipart/form-data
+     * @param MarkdownBuilder|CURLFile|string $voice
+     * @return static
+     */
+    public static function create(MarkdownBuilder|CURLFile|string $voice)
+    {
+        return new static($voice);
+    }
+
+
+
+    /**
+     * Send the Audio message
+     * @return mixed
+     */
+    public function send()
+    {
+        return Bot::sendAudio($this);
     }
 }
